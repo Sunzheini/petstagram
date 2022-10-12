8ea0f026c7719f24991dc49518d849fb10cfa3b4
Sunzheini <daniel_zorov@abv.bg>
2022-10-12 14:47:59 +0300

    Update12102022

--- petstagram/pets/urls.py
+++ petstagram/pets/urls.py
@@ -6,7 +6,7 @@ from petstagram.pets.views import add_pet, delete_pet, details_pet, edit_pet
 urlpatterns = (
     # http://127.0.0.1:8000/pets/add/
     path('add/', add_pet, name='add pet'),
-    path('<str:username>/pet/<slug:pet_name>/', include([
+    path('<str:username>/pet/<slug:pet_slug>/', include([
         # http://127.0.0.1:8000/pets/daniel/pet/maxi/delete/
         path('delete/', delete_pet, name='delete pet'),
         # http://127.0.0.1:8000/pets/daniel/pet/maxi/
